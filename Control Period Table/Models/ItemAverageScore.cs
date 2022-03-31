51755962d617afbf1bcdf3bef68197e354535a92
PinPex <aleksei-picnhuk0202@mail.ru>
2022-03-31 16:40:21 +0700

    load_files

---
+++ Control Period Table/Models/ItemAverageScore.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Control_Period_Table.Models
+{
+    public class ItemAverageScore : INotifyPropertyChanged
+    {
+        string score;
+        string color;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ItemAverageScore()
+        {
+            score = "0";
+            color = "Red";
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Score 
+        {
+            get => score;
+            set
+            {
+                score = value;
+                try
+                {
+                    if (Convert.ToDouble(score) < 1)
+                        Color = "Red";
+                    else if (Convert.ToDouble(score) < 1.5)
+                        Color = "Yellow";
+                    else
+                        Color = "LightGreen";
+                    NotifyPropertyChanged();
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        public string Color
+        {
+            get => color;
+            set
+            {
+                color = value;
+                NotifyPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Control Period Table/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using System.Reactive;
+using Control_Period_Table.Models;
+
+namespace Control_Period_Table.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        List<string> items;
+        List<Student> students;
+        List<ItemAverageScore> itemsAverage = new List<ItemAverageScore>() { new ItemAverageScore(), new ItemAverageScore(), new ItemAverageScore() };
+
+        ObservableCollection<Student> studentList;
+        ObservableCollection<ItemAverageScore> itemsAverageList;
+        int stud_num = 1;
+        string stud_name = "Студент №1";
+
+        public MainWindowViewModel()
+        {
+            items = new List<string>() { "Математика", "Визуальное программирование", "СиАОД" };
+            students = new List<Student>() { };
+            studentList = new ObservableCollection<Student>(students);
+            itemsAverageList = new ObservableCollection<ItemAverageScore>(itemsAverage);
+            Add = ReactiveCommand.Create(() => add());
+            RemoveSelected = ReactiveCommand.Create(() => Remove());
+        }
+
+        public void add()
+        {
+            students.Add(new Student(items, stud_name));
+            stud_num++;
+            stud_name = "Студент №" + Convert.ToString(stud_num);
+            StudentList = new ObservableCollection<Student>(students);
+            RefreshAverageList();
+        }
+
+        public void Remove()
+        {
+            List<Student> removeList = new List<Student>();
+            foreach (var item in StudentList)
+            {
+                if(item.IsSelected)
+                    removeList.Add(item);
+            }
+            foreach (var item in removeList)
+            {
+                students.Remove(item);
+            }
+            StudentList = new ObservableCollection<Student>(students);
+            RefreshAverageList();
+        }
+
+        public void RefreshAverageList()
+        {
+            try
+            {
+                foreach(ItemAverageScore average in itemsAverage)
+                {
+                    average.Score = "0";
+                }
+
+                foreach (Student student in studentList)
+                {
+                    for (int i = 0; i < student.ItemList.Count(); i++)
+                    {
+                        itemsAverageList[i].Score = Convert.ToString(Convert.ToDouble(itemsAverageList[i].Score) 
+                            + Convert.ToDouble(student.ItemList[i].Score) / (double)studentList.Count());
+                    }
+                }
+            }
+            catch
+            {
+                //foreach (ItemAverageScore average in itemsAverage)
+                //{
+                //    average.Score = 0;
+                //}
+            }
+        }
+
+        public void RefreshStudentAverage()
+        {
+            foreach (var student in studentList)
+            {
+                student.RefreshAverage();
+            }
+        }
+
+        public ReactiveCommand<Unit, Unit> Add { get; }
+        public ReactiveCommand<Unit, Unit> RemoveSelected { get; }
+        public ObservableCollection<Student> StudentList
+        {
+            get => studentList;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref studentList, value); 
+            }
+        }
+
+        public ObservableCollection<ItemAverageScore> ItemsAverageList 
+        {
+            get => itemsAverageList;
+        }
+
+        public List<Student> Students
+        {
+            get => students;
+        }
+
+        public void SaveFile(string path)
+        {
+            ProcessingFile.WriteFile(path, students);
+        }
+
+        public void LoadFile(string path)
+        {
+            students = ProcessingFile.ReadFile(path, items.Count());
+            StudentList = new ObservableCollection<Student>(students);
+            RefreshAverageList();
+            RefreshStudentAverage();
+        }
+    }
+}
